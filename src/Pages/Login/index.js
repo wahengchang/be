50ff2388101368e44d1e24ba4b149950304f78c4
waheng <peterchang@yahoo-inc.com>
2018-04-19 22:46:51 +0800

    Added email, password form submit

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './index.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (field) => (e) => {
+    this.setState({[field]: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    console.log(' ------ handleSubmit ------')
+    console.log(this.state)
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +31,18 @@ class Login extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="formWarpper">
+          <form className="pure-form pure-form-stacked" onSubmit={this.handleSubmit.bind(this)}>
+              <fieldset>
+                  <legend>A Stacked Form</legend>
+                  <input id="email" type="email" placeholder="Email" value={this.state.email} onChange={this.handleChange('email')}/>
+                  <span className="pure-form-message">This is a required field.</span>
+                  <input id="password" type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange('password')}/>
+                  <button type="submit" className="pure-button pure-button-primary">Sign in</button>
+              </fieldset>
+          </form>
+        </div>
       </div>
     );
   }
