888d3478d251fa54a9d50fc5330bf9ec6970897b
waheng <peterchang@yahoo-inc.com>
2018-05-24 18:36:28 +0800

    no message

--- .travis/unit-test-fail.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-   echo "1- unit test fail"
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-   echo "2- unit test fail"
-fi
---
+++ travis/unit-test-fail.sh
@@ -0,0 +1,11 @@
+curl -H "Authorization: token ${github_token}" -X POST \
+-d "{\"body\": \"Hello world\"}" \
+"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   echo "1- unit test fail"
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+   echo "2- unit test fail"
+fi
