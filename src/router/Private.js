b7fe91aebca58a926c9601dc5829998f338afcb0
StevenLeeTW <s1365789@gmail.com>
2018-05-27 00:13:51 +0800

    add prettier

--- src/router/Private.js
+++ src/router/Private.js
@@ -1,22 +1,20 @@
 /*eslint-disable no-unused-vars*/
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Container extends Component {
-  render () {
-    const {currentUser} = this.props
+  render() {
+    const { currentUser } = this.props;
     if (currentUser && currentUser.isLogin) {
-      return this.props.children
+      return this.props.children;
     } else {
       return null
     }
   }
 }
 
-function mapStateToProps(state){
-  return {currentUser: state.currentUser}
+function mapStateToProps(state) {
+  return { currentUser: state.currentUser };
 }
 
-export default connect(
-  mapStateToProps
-)(Container)
+export default connect(mapStateToProps)(Container);
