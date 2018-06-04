f3c4e8fc774018ca9e01b7105c826e79b927d14d
StevenLeeTW <s1365789@gmail.com>
2018-06-04 23:54:37 +0800

    add image sideBtn

--- src/Components/EditorPage/Editor/index.js
+++ src/Components/EditorPage/Editor/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Editor, createEditorState } from 'medium-draft'
 import './index.css'
+import ImageSideBtn from './ImageSideBtn'
 
 class EditorComponent extends React.Component {
   constructor(props) {
@@ -9,10 +10,17 @@ class EditorComponent extends React.Component {
     this.state = {
       editorState: createEditorState(rawContent)
     }
+
+    this.sideButtons = [
+      {
+        title: 'Image',
+        component: ImageSideBtn
+      }
+    ]
   }
 
   componentDidMount() {
-    this.editor.focus()
+    this.refs.editor.focus()
   }
 
   onChange = editorState => {
@@ -25,13 +33,13 @@ class EditorComponent extends React.Component {
       <div className="editorLayout">
         <div className="editorComponentWrapper">
           <Editor
-            ref={element => {
-              this.editor = element
-            }}
+            ref="editor"
             editorState={editorState}
             onChange={this.onChange}
+            sideButtons={this.sideButtons}
           />
         </div>
+        <div className="uploadBar" />
       </div>
     )
   }
