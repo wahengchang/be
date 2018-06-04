e40ae88563bd260efeeb603dee7fee9525670483
waheng chang <wahengchang@gmail.com>
2018-06-05 00:02:03 +0900

    Update: story book

--- stories/Editor.js
+++ stories/Editor.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import EditorComponent from '../src/Components/EditorPage/Editor'
+import DraftJS, {EditorState} from 'draft-js'
 
 const style = {
   border: 'black solid 1px',
@@ -10,8 +11,17 @@ const style = {
 }
 
 storiesOf('MyEditor', module)
-.add('with some emoji', () => (
-  <div style={style}>
-    <EditorComponent/>
+.add('with some emoji', () => {
+  const emptyState = EditorState.createEmpty()
+  
+  const storyData = {
+    rawContent: DraftJS.convertToRaw(emptyState.getCurrentContent())
+  }
+
+  return <div style={style}>
+    <EditorComponent
+      storyData = {storyData}
+    />
   </div>
-));
+
+});
