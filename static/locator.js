fae8d3d9aec769e0ab130df00929df3e69bf3da7
waheng chang <wahengchang@gmail.com>
2018-08-24 23:06:17 +0800

    80b1594

---
+++ static/locator.js
@@ -0,0 +1,8 @@
+
+        window.FIREBASE_API_KEY =  "AIzaSyAWLSaDUjTBwfF181sJNgN5RI5X2nNF9CA"
+        window.FIREBASE_AUTH_DOMAIN =  "baseball-dev.firebaseapp.com"
+        window.FIREBASE_DATABASE_URL =  "https://baseball-dev.firebaseio.com"
+        window.FIREBASE_PROJECT_ID =  "baseball-dev"
+        window.FIREBASE_STORAGE_BUCKET =  "baseball-dev.appspot.com"
+        window.FIREBASE_MESSAGING_SENDER_ID =  "211794632987"
+    
